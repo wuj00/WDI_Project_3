9cadbb9df32005e6acf63faac49f82aa014ff522
Nicole Thurnau <nthurnau@gmail.com>
2016-03-12 21:40:53 -0800

    added user model with user schema, also added more functionality to the profile page

--- Models/user.js
+++ Models/user.js
@@ -1,2 +1,30 @@
-var
-  mongoose = require('mongoose')
+var mongoose = require('mongoose')
+var bcrypt = require('bcrypt-nodejs')
+var Schema = mongoose.Schema
+
+var userSchema = new Schema({
+  local: {
+    name: String,
+    email: String,
+    password: String,
+    description: String
+  },
+  facebook: { //facebook stratgey - passwords already exist, we need their fb id
+    id: String,
+    name: String,
+    token: String, //
+    email: String,
+    description: String
+  }
+})
+userSchema.methods.generateHash = function(password){
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8))
+}
+
+userSchema.methods.generateHash = function(password){
+  return bcrypt.compareSync(password, this.local.password)
+}
+
+var User = mongoose.model('User', userSchema)
+
+module.exports = User 
