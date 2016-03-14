32d7b91659430474dda82f734a67d45050f81ee0
Nicole Thurnau <nthurnau@gmail.com>
2016-03-14 16:32:57 -0700

    added routes for FB users and FB authentication to passport.js

--- routes/main_routes.js
+++ routes/main_routes.js
@@ -2,39 +2,57 @@ var express = require ('express')
 var passport = require('passport')
 var router = express.Router()
 
-//*********************map route**********************************
+//*********************map route************************************************
 router.get('/map', function(req, res){
   res.render('index')
 })
-//********************login route AND signup**********************
+//********************login route AND signup************************************
 router.get('/login', function(req, res){
   res.render('landing_page', {message: req.flash('loginMessage')})
 })
-//*****************create session using passport*******************
+//*****************create session using passport********************************
 router.post('/login', passport.authenticate('local-login', {
   successRedirect: '/map', //if success go to the map
   failureRedirect: '/login' //if failure, redirect to login
 }))
-//********************create new user******************************
+//********************create new user*******************************************
 router.post('/signup', passport.authenticate('local-signup', { //submission is being sent to a unique route, where you end up doesn't matter
   successRedirect: '/profile',
   failureRedirect: '/login'
 }))
-//************************profile route****************************
-router.get('/profile', function(req, res){
-  res.render('profile')
-})
+//************************profile route*****************************************
+// router.get('/profile', function(req, res){
+//   res.render('profile')
+// })
 //this is the authentication version of the profile route, add that later
-router.get('/profile', function(rep, res){
+router.get('/profile', isLoggedIn, function(req, res){
+  res.render('profile', {user: req.user})
+})
+//this is the authentication version of the map route
+//****************************authorized map view*******************************
+router.get('/map', isLoggedIn, function(req, res){
+  res.render('map', {user: req.user})
+})
+//****************************logout route**************************************
+router.get('/logout', function(req, res){
   req.logout()
   res.redirect('/login')
 })
+//***********************Part 1 signup route for Facebook Users*****************
+router.get('auth/facebook', passport.authenticate('facebook',{
+  scope: ['email']
+}))
+//************************Part 2signup route for Facebook Users*****************
+router.get('/auth/facebook/callback', passport.authenticate('facebook',{
+    successRedirect: '/profile',
+    failureRedirect: '/login'
+}))
 
 //**middleware that checks to see if user is logged in and is that user**
 //uncomment this when ready to test!
-// function isLoggedIn(req, res, next){
-//   if(req.isAuthenticated()) return next()
-//   res.redirect('/login')
-// }
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()) return next()
+  res.redirect('/login')
+}
 
 module.exports = router
