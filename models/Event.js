6b3a51081e00a970f645bd6125926fddc172372a
lewhub <bracey.lewis@gmail.com>
2016-03-16 20:02:23 -0700

    changed default on rsvp options

--- models/Event.js
+++ models/Event.js
@@ -5,8 +5,8 @@ var eventSchema = new mongoose.Schema({
   time: String,
   how_many_buds: Number,
   specific_location: String,
-  going_buds: Number,
-  maybe_buds: Number,
+  going_buds: {type: Number, default: 0},
+  maybe_buds: {type: Number, default: 0},
   _created_by: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
   _location: {type: mongoose.Schema.Types.ObjectId, ref: 'Spot'}
 })
