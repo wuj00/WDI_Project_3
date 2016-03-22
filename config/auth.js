9789e0c95567ae792683cad11b0db842741b23f5
lewhub <bracey.lewis@gmail.com>
2016-03-21 19:20:51 -0700

    facebook redirect corrected in passport

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
   'facebook':{
     'clientID': process.env.FACEBOOK_CLIENT_ID,
     'clientSecret': process.env.FACEBOOK_CLIENT_SECRET,
-    'callbackURL': 'http://localhost:3000/auth/facebook/callback' || 'https://fathomless-tundra-33285.herokuapp.com/auth/facebook/callback',
+    'callbackURL': 'https://fathomless-tundra-33285.herokuapp.com/auth/facebook/callback', // 'http://localhost:3000/auth/facebook/callback' || 
     'profileFields': ['emails', 'displayName']
   }
 }
